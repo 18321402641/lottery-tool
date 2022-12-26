9fc223af5a67cd438b5e5296883d09aa30293b19
szy <szy@qq.com>
2022-12-26 09:54:57 +0800

    1

--- js/data/data-lucky.js
+++ js/data/data-lucky.js
@@ -2,7 +2,7 @@
  * Created by dell on 2017/12/19.
  */
 define([
-    {id:"004",name:"花花",company:"阿联酉集团"},
-    {id:"005",name:"略略",company:"斯巴达集团"},
-    {id:"006",name:"润润",company:"帝国集团"}
+    {id:"004",name:"Glenn",company:"29"},
+    {id:"005",name:"Jack",company:"30"},
+    {id:"006",name:"Alex",company:"12"}
 ]);
